5d48d24a7ba924b6b17daf1a550ee7d6a185a687
Victor Koome <koomev786@gmail.com>
2024-02-27 22:25:55 +0300

    Add Default values for props

--- src/index.js
+++ src/index.js
@@ -1,13 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 // import "./index.css";
 // import App from "./App";
 import StarRating from "./StarRating";
 
+function Test() {
+  const [songRating, setSongRating] = useState(0);
+  return (
+    <div>
+      <StarRating
+        maxRating={10}
+        color="green"
+        onSetSongRating={setSongRating}
+      />
+      <p>You rated this song {songRating}</p>
+    </div>
+  );
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
+    <StarRating
+      maxRating={5}
+      className="test"
+      messages={["terrible", "bad", "average", "good", "exellent"]}
+    />
+    <StarRating size={24} color="red" defaultRating={2} />
+    <Test />
   </React.StrictMode>
 );
